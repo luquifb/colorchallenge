d053d6ff79d0a86da24d5d1688ec12e25cc5cb3f
Lucas <lfbodereau@gmail.com>
2017-09-01 12:25:18 +0200

    clean final code

--- js/app.js
+++ js/app.js
@@ -1,11 +1,7 @@
 
 // Begin Game
 
-function newGame () {
-    // this.response = response;
-    // this.operator = operator;
-    // this.answer = answer;
-    
+function newGame () {    
     return getColorQuestion(3);
 }
 
@@ -22,10 +18,6 @@ $(document).ready(function(){
 // Generate Colors in circles
 
 var response = newGame();
-    // console.log(response.question.a.name)
-    // console.log(response.question.b.name)
-    // console.log(response);
-
 var color1 = response.question.a.name;
     $('.position-1').addClass(color1);
 var color2 = response.question.b.name;
@@ -33,10 +25,6 @@ var color2 = response.question.b.name;
 
 
 // Generate answer in options
-
-// var answer = newGame();
-//     console.log(response.question.answer.name)
-//     console.log(response);
 
 var result = Math.floor(Math.random()*3)+1;
 var colorComb = response.question.answer.name;
@@ -54,7 +42,9 @@ var option = $('.option'+result).addClass('correct').text(colorComb);
         $('.option3').text(getRandomColors)
     }
 
+
 // Random Colors Option
+
 function getRandomColors() {
     var letters = '0F';
     var color = '#';
@@ -82,18 +72,6 @@ function generateHexColor() {
         generateHexColor();
    
 
-
-    // for(var i = 0; i < 3; i++){
-    //     var colorComb = response.question.answer.name;
-
-    //     var option = $('<button>').addClass('correct').attr('option',colorComb);
-    //         option.click(function(e){
-
-    //         $(this).attr('option').attr('option',colorComb).text(colorComb);
-    //         console.log('has hecho click en la opción ' + option);
-    //     })
-    // }
-
 // Print operator in question
 
 var operator = newGame();
@@ -112,87 +90,45 @@ $('#options').on('click', function(e){
     }
 });
 
+
 // Ask for help
+
 $('.btn-info').on('click', function(){
     return alert("I'm sorry, try harder!!")
 });
 
 
-
-// Timer 
-
-// var startTimer = function(){
-//     timer = setInterval(function(){
-//         timePassed += 1000;
-//         var minutes = Math.floor(timePassed/(1000*60));
-//         var seconds = (timePassed/1000)%60;
-//         if(seconds < 10) {
-//             var timeDisplay = minutes + ":0" + seconds;
-//         } else {
-//             var timeDisplay = minutes + ":" + seconds;
-//         }
-//         $("#timer").text(timeDisplay);
-//     },1000);
-// }
-
-// function returnMessage(){
-
-// };
-
-// Counter 
-
-// // var count = 0;
-// // for(var i = 0; i < array.length; ++i){
-// //     if(array[i] == 2)
-// //         count++;
-// // }
-
-
-
 });
 
 
-
-
-    // for(var i = 0; i < 3; i++){
-    //     var colorComb = response.question.answer.name;
-
-    //     var option = $('<button>').addClass('correct').attr('option',colorComb);
-    //         option.click(function(e){
-
-    //         $(this).attr('option').attr('option',colorComb).text(colorComb);
-    //         console.log('has hecho click en la opción ' + option);
-    //     })
-    // }
-
-
-    // Balls changing colors random
-        window.setInterval(function(){
-            
-                var randomColor = '#'+ ('000000' + Math.floor(Math.random()*16777215).toString(16)).slice(-6);
-                
-                $('.border').css({
-                'background-color' : randomColor,
-                });
-            
-            }, 500);
-
-      window.setInterval(function(){
+// Balls changing colors random
+   
+window.setInterval(function(){
         
-            var randomColor = '#'+ ('000000' + Math.floor(Math.random()*16777215).toString(16)).slice(-6);
-            
-            $('.border2').css({
-              'background-color' : randomColor,
-            });
-        
-          }, 500);      
+    var randomColor = '#'+ ('000000' + Math.floor(Math.random()*16777215).toString(16)).slice(-6);
+    
+        $('.border').css({
+        'background-color' : randomColor,
+        });
 
-     window.setInterval(function(){
-            
-          var randomColor = '#'+ ('000000' + Math.floor(Math.random()*16777215).toString(16)).slice(-6);
-          
-          $('.border3').css({
-            'background-color' : randomColor,
-          });
+        }, 500);
+
+window.setInterval(function(){
+  
+    var randomColor = '#'+ ('000000' + Math.floor(Math.random()*16777215).toString(16)).slice(-6);
       
+        $('.border2').css({
+            'background-color' : randomColor,
+        });
+    
+        }, 500);      
+
+window.setInterval(function(){
+       
+    var randomColor = '#'+ ('000000' + Math.floor(Math.random()*16777215).toString(16)).slice(-6);
+     
+        $('.border3').css({
+        'background-color' : randomColor,
+        });
+    
         }, 500);  
